a0de1cfe12d577a14cba51f589a60b8a510d2bff
Projet_exam <florian.leininger01@gmail.com>
2023-04-14 13:37:54 +0200

    + liste catégories forum

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Categorie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ForumController extends AbstractController
 {
     #[Route('/forum', name: 'app_forum')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $categories = $doctrine->getRepository(Categorie::class)->findBy(
+            [],
+            ['name' => 'ASC']
+        );
         return $this->render('forum/index.html.twig', [
-            'controller_name' => 'ForumController',
+            'categories' => $categories,
         ]);
     }
 }
