ba337c7132bcf32ff446b67f4b57f910bafdcf90
Exo_symfony <florian.leininger01@gmail.com>
2023-05-01 10:16:44 +0200

    + affichage messages

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -252,4 +252,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString() {
+        return $this->getPseudo();
+    }
 }
