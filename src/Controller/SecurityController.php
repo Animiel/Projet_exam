674c037ade2da6f94f971353d41d42980d25ea12
Projet_exam <florian.leininger01@gmail.com>
2023-06-26 14:52:44 +0200

    + ajout bannir user + suppr elements

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Entity\Sujet;
+use App\Entity\Annonce;
+use App\Entity\Message;
+use App\Entity\Categorie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -28,5 +34,56 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/bannir/{id}', name: 'ban_user')]
+    public function bannir(User $user, ManagerRegistry $doctrine)
+    {
+        $user->setBanni(1);
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/supprMsg/{id}', name: 'suppr_msg')]
+    public function supprMsg(ManagerRegistry $doctrine, Message $msg)
+    {
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($msg);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/supprAnnonce/{id}', name: 'suppr_annonce')]
+    public function supprAnnonce(ManagerRegistry $doctrine, Annonce $annonce)
+    {
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($annonce);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/supprSujet/{id}', name: 'suppr_sujet')]
+    public function supprSujet(ManagerRegistry $doctrine, Sujet $sujet)
+    {
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($sujet);
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/supprCtg/{id}', name: 'suppr_ctg')]
+    public function supprCtg(ManagerRegistry $doctrine, Categorie $categorie)
+    {
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($categorie);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
     }
 }
