46aebc1d92af7945fabc8a78b6b5e144d0e83501
Exo_symfony <florian.leininger01@gmail.com>
2023-06-30 16:30:13 +0200

    + amélioration slider

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,26 @@
-var counter = 1;
-setInterval(function() {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 5) {
-        counter = 1;
+let t_slider = document.getElementsByClassName('t_slider');
+let etape = 0;
+let nbr_slides = t_slider.length;
+let buttons = "";
+
+function removeActive() {
+    for(let i = 0 ; i < nbr_slides ; i++) {
+        t_slider[i].classList.remove('active');
     }
+}
+
+setInterval(function() {
+    // document.getElementById('radio' + counter).checked = true;
+    etape++;
+    if(etape >= nbr_slides) {
+        etape = 0;
+    }
+    removeActive();
+    t_slider[etape].classList.add('active');
 }, 5000);
+
+// window.addEventListener("load", (event) => {
+//     for(let j = 0 ; j < nbr_slides ; j++) {
+//         document.body.insertBefore(document.createElement('input'), document.getElementById('bloc'));
+//     }
+//   });
