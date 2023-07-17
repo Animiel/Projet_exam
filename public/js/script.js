d714c1afd0862fa7461190c6bb340c801aa6b611
projet_exam <florian.leininger01@gmail.com>
2023-07-17 14:19:39 +0200

    + flip card button

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,6 @@
 let t_slider = document.getElementsByClassName('t_slider');
 let etape = 0;
 let nbr_slides = t_slider.length;
-let buttons = "";
 
 function removeActive() {
     for(let i = 0 ; i < nbr_slides ; i++) {
@@ -34,10 +33,14 @@ function onClick(element) {
 
 const coeurPlein = document.getElementById('solid-off');
 const coeurVide = document.getElementById('regular-on');
+let cardInfos = document.getElementsByClassName('dogInfos');
 
 window.addEventListener('load', (event) => {
     coeurPlein.style.left = '-20px';
     coeurVide.style.left = '10px';
+    for (let dogIndex = 0; dogIndex < cardInfos.length; dogIndex++) {
+        cardInfos[dogIndex].style.display = 'none';
+    }
 }); 
 
 coeurVide.addEventListener('mouseover', function() {
@@ -85,35 +88,52 @@ function removeActiveImg() {
 //     slider_children[actImg].classList.add('activeImg');
 // }
 
-for (let j = 1; j <= cards.length; j++) {
-    a_slider.push(document.getElementsByClassName(`first-view-${j}`));  //a_slider == [[]] => a_slider[j] = []
-}
-for (let k = 0; k < a_slider.length; k++) {
-    let slider_children = a_slider[k][0].children;
-    // console.log(slider_children);
-    for (let l = 0; l < slider_children.length; l++) {
-        if (slider_children[l].localName == "img") {
-            a_slides.push(slider_children[l])
-        }
-        if (slider_children[l].className == 'fa-caret-left') {
-            slider_children[l].addEventListener('click', element => {
-                if(actImg <= 0) {
-                    actImg = nbr_img;
-                }
-                actImg--;
-                removeActiveImg();
-                slider_children[actImg].classList.add('activeImg');
-            });
-        }
-        if (slider_children[l].className == 'fa-caret-right') {
-            slider_children[l].addEventListener('click', element => {
-                actImg++;
-                if(actImg >= nbr_img) {
-                    actImg = 0;
-                }
-                removeActiveImg();
-                slider_children[actImg].classList.add('activeImg');
-            });
-        }
-    }
+// for (let j = 1; j <= cards.length; j++) {
+//     a_slider.push(document.getElementsByClassName(`first-view-${j}`));  //a_slider == [[]] => a_slider[j] = []
+// }
+// for (let k = 0; k < a_slider.length; k++) {
+//     let slider_children = a_slider[k][0].children;
+//     // console.log(slider_children);
+//     for (let l = 0; l < slider_children.length; l++) {
+//         if (slider_children[l].localName == "img") {
+//             a_slides.push(slider_children[l])
+//         }
+//         if (slider_children[l].className == 'fa-caret-left') {
+//             slider_children[l].addEventListener('click', element => {
+//                 if(actImg <= 0) {
+//                     actImg = nbr_img;
+//                 }
+//                 actImg--;
+//                 removeActiveImg();
+//                 slider_children[actImg].classList.add('activeImg');
+//             });
+//         }
+//         if (slider_children[l].className == 'fa-caret-right') {
+//             slider_children[l].addEventListener('click', element => {
+//                 actImg++;
+//                 if(actImg >= nbr_img) {
+//                     actImg = 0;
+//                 }
+//                 removeActiveImg();
+//                 slider_children[actImg].classList.add('activeImg');
+//             });
+//         }
+//     }
+// }
+
+
+
+
+
+for (let numAnnonce = 1; numAnnonce < cards.length; numAnnonce++) {
+    let btnDogInfo = document.getElementById(`info-${numAnnonce}`);
+    let btnBackInfo = document.getElementById(`back-${numAnnonce}`);
+    btnDogInfo.addEventListener('click', element => {
+        document.getElementById(`second-view-${numAnnonce}`).style.display = 'block';
+        document.getElementById(`first-view-${numAnnonce}`).style.display = 'none';
+    });
+    btnBackInfo.addEventListener('click', element => {
+        document.getElementById(`second-view-${numAnnonce}`).style.display = 'none';
+        document.getElementById(`first-view-${numAnnonce}`).style.display = 'block';
+    });
 }
