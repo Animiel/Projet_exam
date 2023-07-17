aac34eb8b950b78cf72977e8a5c90b8abe827418
projet_exam <florian.leininger01@gmail.com>
2023-07-17 13:23:39 +0200

    + continuation slider annonce

--- public/js/script.js
+++ public/js/script.js
@@ -32,32 +32,30 @@ function onClick(element) {
 
 
 
-let cards = document.getElementsByClassName('wanted');
 const coeurPlein = document.getElementById('solid-off');
 const coeurVide = document.getElementById('regular-on');
 
 window.addEventListener('load', (event) => {
     coeurPlein.style.left = '-20px';
     coeurVide.style.left = '10px';
-  }); 
+}); 
 
 coeurVide.addEventListener('mouseover', function() {
     coeurVide.animate([
-        { transform: 'translateX(-40px)' }],
-        { duration: 600});
+    { transform: 'translateX(-40px)' }],
+    { duration: 600});
     coeurPlein.animate([
-        { transform: 'translateX(30px)' }],
-        { duration: 600});
-    coeurPlein.addEventListener('mouseover', function() {
-        setTimeout(5000);
-    })
+    { transform: 'translateX(30px)' }],
+    { duration: 600});
 })
 
 
 
 
 
-let a_slides = document.getElementsByClassName('wanted-pic');
+let cards = document.getElementsByClassName('wanted');
+let a_slides = [];
+let a_slider = [];
 let actImg = 0;
 let nbr_img = a_slides.length;
 let leftArrow = document.getElementsByClassName('fa-caret-left');
@@ -69,24 +67,53 @@ function removeActiveImg() {
     }
 }
 
-function previous() {
-    actImg--;
-    if(actImg < 0) {
-        actImg = nbr_img;
-    }
-    removeActiveImg();
-    a_slides[actImg].classList.add('activeImg');
-}
+// function previous() {
+//     if(actImg <= 0) {
+//         actImg = nbr_img;
+//     }
+//     actImg--;
+//     removeActiveImg();
+//     slider_children[actImg].classList.add('activeImg');
+// }
 
-function next() {
-    actImg++;
-    if(actImg >= nbr_img) {
-        actImg = 0;
-    }
-    removeActiveImg();
-    a_slides[actImg].classList.add('activeImg');
+// function next() {
+//     actImg++;
+//     if(actImg >= nbr_img) {
+//         actImg = 0;
+//     }
+//     removeActiveImg();
+//     slider_children[actImg].classList.add('activeImg');
+// }
+
+for (let j = 1; j <= cards.length; j++) {
+    a_slider.push(document.getElementsByClassName(`first-view-${j}`));  //a_slider == [[]] => a_slider[j] = []
 }
-console.log(cards);
-console.log(leftArrow);
-console.log(a_slides);
-console.log(cards.a_slides);
+for (let k = 0; k < a_slider.length; k++) {
+    let slider_children = a_slider[k][0].children;
+    // console.log(slider_children);
+    for (let l = 0; l < slider_children.length; l++) {
+        if (slider_children[l].localName == "img") {
+            a_slides.push(slider_children[l])
+        }
+        if (slider_children[l].className == 'fa-caret-left') {
+            slider_children[l].addEventListener('click', element => {
+                if(actImg <= 0) {
+                    actImg = nbr_img;
+                }
+                actImg--;
+                removeActiveImg();
+                slider_children[actImg].classList.add('activeImg');
+            });
+        }
+        if (slider_children[l].className == 'fa-caret-right') {
+            slider_children[l].addEventListener('click', element => {
+                actImg++;
+                if(actImg >= nbr_img) {
+                    actImg = 0;
+                }
+                removeActiveImg();
+                slider_children[actImg].classList.add('activeImg');
+            });
+        }
+    }
+}
