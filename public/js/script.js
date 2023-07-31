bb321a868aab18a522c0fad68675d67aa60a344e
projet_exam <florian.leininger01@gmail.com>
2023-07-31 09:47:15 +0200

    + rogner 1 image à la création annonce

--- public/js/script.js
+++ public/js/script.js
@@ -106,43 +106,43 @@ for (let j = 1; j <= cards.length; j++) {
 
 
 
-let gallery = document.getElementById('gallery-grid');
-let imagesGallery = document.getElementsByClassName('gallery-img');
-let currentImg = 0;
+// let gallery = document.getElementById('gallery-grid');
+// let imagesGallery = document.getElementsByClassName('gallery-img');
+// let currentImg = 0;
 
-window.addEventListener('load', element => {
-    imagesGallery[0].classList.add('currentGalImg');
-    imagesGallery[1].classList.add('currentGalImg');
-    imagesGallery[2].classList.add('currentGalImg');
-})
-setInterval(function() {
-    currentImg++;
-    if (imagesGallery[currentImg + 2] >= imagesGallery[imagesGallery.length]) {
-        imagesGallery[currentImg + 2] = imagesGallery[0];
-        imagesGallery[0].classList.add('currentGalImg');
-        imagesGallery[currentImg + 1] = imagesGallery[imagesGallery.length];
-        imagesGallery[imagesGallery.length].classList.add('currentGalImg');
-    }
-    if (imagesGallery[currentImg + 1] >= imagesGallery[imagesGallery.length]) {
-        imagesGallery[currentImg + 2] = imagesGallery[1];
-        imagesGallery[1].classList.add('currentGalImg');
-        imagesGallery[currentImg + 1] = imagesGallery[0];
-        imagesGallery[0].classList.add('currentGalImg');
-    }
-    if (imagesGallery[currentImg] == imagesGallery[imagesGallery.length]) {
-        imagesGallery[currentImg + 2] = imagesGallery[2];
-        imagesGallery[2].classList.add('currentGalImg');
-        imagesGallery[currentImg + 1] = imagesGallery[1];
-        imagesGallery[1].classList.add('currentGalImg');
-    }
-    imagesGallery[currentImg - 1].classList.remove('currentGalImg');
-    imagesGallery[currentImg] = imagesGallery[currentImg + 1];
-    imagesGallery[currentImg].classList.add('currentGalImg');
-    imagesGallery[currentImg + 1] = imagesGallery[currentImg + 2];
-    imagesGallery[currentImg + 1].classList.add('currentGalImg');
-    imagesGallery[currentImg + 2] = imagesGallery[currentImg + 3];
-    imagesGallery[currentImg + 2].classList.add('currentGalImg');
-}, 4000);
+// window.addEventListener('load', element => {
+//     imagesGallery[0].classList.add('currentGalImg');
+//     imagesGallery[1].classList.add('currentGalImg');
+//     imagesGallery[2].classList.add('currentGalImg');
+// })
+// setInterval(function() {
+//     currentImg++;
+//     if (imagesGallery[currentImg + 2] >= imagesGallery[imagesGallery.length]) {
+//         imagesGallery[currentImg + 2] = imagesGallery[0];
+//         imagesGallery[0].classList.add('currentGalImg');
+//         imagesGallery[currentImg + 1] = imagesGallery[imagesGallery.length];
+//         imagesGallery[imagesGallery.length].classList.add('currentGalImg');
+//     }
+//     if (imagesGallery[currentImg + 1] >= imagesGallery[imagesGallery.length]) {
+//         imagesGallery[currentImg + 2] = imagesGallery[1];
+//         imagesGallery[1].classList.add('currentGalImg');
+//         imagesGallery[currentImg + 1] = imagesGallery[0];
+//         imagesGallery[0].classList.add('currentGalImg');
+//     }
+//     if (imagesGallery[currentImg] == imagesGallery[imagesGallery.length]) {
+//         imagesGallery[currentImg + 2] = imagesGallery[2];
+//         imagesGallery[2].classList.add('currentGalImg');
+//         imagesGallery[currentImg + 1] = imagesGallery[1];
+//         imagesGallery[1].classList.add('currentGalImg');
+//     }
+//     imagesGallery[currentImg - 1].classList.remove('currentGalImg');
+//     imagesGallery[currentImg] = imagesGallery[currentImg + 1];
+//     imagesGallery[currentImg].classList.add('currentGalImg');
+//     imagesGallery[currentImg + 1] = imagesGallery[currentImg + 2];
+//     imagesGallery[currentImg + 1].classList.add('currentGalImg');
+//     imagesGallery[currentImg + 2] = imagesGallery[currentImg + 3];
+//     imagesGallery[currentImg + 2].classList.add('currentGalImg');
+// }, 4000);
 
 
 
@@ -153,3 +153,75 @@ function showInfo(element) {
     document.getElementById(`info-modal-${divIndex}`).style.display = "block";
     document.getElementById(`second-view-${divIndex}`).style.display = "block";
 }
+
+
+
+
+
+// image-box is the id of the div element that will store our cropping image preview
+const imagebox = document.getElementById('image-cropbox')
+    // crop-btn is the id of button that will trigger the event of change original file with cropped file.
+const crop_btn = document.getElementById('crop-btn')
+// id_image is the id of the input tag where we will upload the image
+const input = document.getElementById('annonce_images')
+
+// When user uploads the image this event will get triggered
+input.addEventListener('change', ()=>{
+    // Getting image file object from the input variable
+    const img_data = input.files[0]
+    // createObjectURL() static method creates a DOMString containing a URL representing the object given in the parameter.
+    // The new object URL represents the specified File object or Blob object.
+    const url = URL.createObjectURL(img_data)
+    
+    // Creating a image tag inside imagebox which will hold the cropping view image(uploaded file) to it using the url created before.
+    imagebox.innerHTML = `<img src="${url}" id="crop-image" style="width:100%;">`
+
+    // Storing that cropping view image in a variable
+    const image = document.getElementById('crop-image')
+
+    // Displaying the image box
+    document.getElementById('image-cropbox').style.display = 'block'
+    // Displaying the Crop buttton
+    document.getElementById('crop-btn').style.display = 'block'
+    // Hiding the Post button
+    document.getElementById('confirm-btn').style.display = 'none'
+
+    // Creating a croper object with the cropping view image
+    // The new Cropper() method will do all the magic and diplay the cropping view and adding cropping functionality on the website
+    // For more settings, check out their official documentation at https://github.com/fengyuanchen/cropperjs
+    const cropper = new Cropper(image, {
+    autoCropArea: 1,
+    viewMode: 1,
+    scalable: false,
+    zoomable: false,
+    movable: false,
+    minCropBoxWidth: 200,
+    minCropBoxHeight: 200,
+    })
+
+    // When crop button is clicked this event will get triggered
+    crop_btn.addEventListener('click', ()=>{
+    // This method coverts the selected cropped image on the cropper canvas into a blob object
+    cropper.getCroppedCanvas().toBlob((blob)=>{
+        
+        // Gets the original image data
+        let fileInputElement = document.getElementById('annonce_images');
+        // Make a new cropped image file using that blob object, image_data.name will make the new file name same as original image
+        let file = new File([blob], img_data.name,{type:"image/*", lastModified:new Date().getTime()});
+        // Create a new container
+        let container = new DataTransfer();
+        // Add the cropped image file to the container
+        container.items.add(file);
+        // Replace the original image file with the new cropped image file
+        fileInputElement.files = container.files;
+
+        // Hide the cropper box
+        document.getElementById('image-cropbox').style.display = 'none'
+        // Hide the crop button
+        document.getElementById('crop-btn').style.display = 'none'
+        // Display the Post button
+        document.getElementById('confirm-btn').style.display = 'block'
+
+        });
+    });
+});	
