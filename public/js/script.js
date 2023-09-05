6f610e5b69f1302d9aeb59461080bf8e42e9062e
projet_exam <florian.leininger01@gmail.com>
2023-09-05 09:40:36 +0200

    + résolution souci JS

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,11 @@
 const flash = document.getElementById('flash')
 const flashClose = document.getElementById('flash-close')
 
-flashClose.addEventListener('click', element => {
-    flash.style.display = 'none';
-})
+if (flash != null) {
+    flashClose.addEventListener('click', element => {
+        flash.style.display = 'none';
+    })
+}
 
 
 
@@ -23,14 +25,16 @@ bars.addEventListener('click', element => {
 const filterIcon = document.getElementById('filterIcon')
 const filterForm = document.getElementById('filterForm')
 
-filterIcon.addEventListener('click', element => {
-    if(filterForm.style.display == "block") {
-        filterForm.style.display = "none";
-    }
-    else {
-        filterForm.style.display = "block";
-    }
-})
+if (filterIcon != null) {
+    filterIcon.addEventListener('click', element => {
+        if(filterForm.style.display == "block") {
+            filterForm.style.display = "none";
+        }
+        else {
+            filterForm.style.display = "block";
+        }
+    })
+}
 
 
 
@@ -193,78 +197,78 @@ function closeModal() {
 
 
 
-// image-box is the id of the div element that will store our cropping image preview
-const imagebox = document.getElementById('image-cropbox')
-    // crop-btn is the id of button that will trigger the event of change original file with cropped file.
-const crop_btn = document.getElementById('crop-btn')
-// id_image is the id of the input tag where we will upload the image
-const input = document.getElementById('annonce_images')
+// // image-box is the id of the div element that will store our cropping image preview
+// const imagebox = document.getElementById('image-cropbox')
+//     // crop-btn is the id of button that will trigger the event of change original file with cropped file.
+// const crop_btn = document.getElementById('crop-btn')
+// // id_image is the id of the input tag where we will upload the image
+// const input = document.getElementById('annonce_images')
 
-// When user uploads the image this event will get triggered
-input.addEventListener('change', () => {
-    for (let cropIndex = 0; cropIndex < input.files.length; cropIndex++) {
+// // When user uploads the image this event will get triggered
+// input.addEventListener('change', () => {
+//     for (let cropIndex = 0; cropIndex < input.files.length; cropIndex++) {
         
-        // Getting image file object from the input variable
-        // const img_data = input.files[0]
-        // createObjectURL() static method creates a DOMString containing a URL representing the object given in the parameter.
-        // The new object URL represents the specified File object or Blob object.
-        // const url = URL.createObjectURL(img_data)
+//         // Getting image file object from the input variable
+//         // const img_data = input.files[0]
+//         // createObjectURL() static method creates a DOMString containing a URL representing the object given in the parameter.
+//         // The new object URL represents the specified File object or Blob object.
+//         // const url = URL.createObjectURL(img_data)
         
-        // Creating a image tag inside imagebox which will hold the cropping view image(uploaded file) to it using the url created before.
-        // imagebox.innerHTML = `<img src="${URL.createObjectURL(input.files[cropIndex])}" id="crop-image" style="width:100%;">`
-        const img = document.createElement("img");
-        img.src = URL.createObjectURL(input.files[cropIndex]);
-        img.id = `crop-image`;
+//         // Creating a image tag inside imagebox which will hold the cropping view image(uploaded file) to it using the url created before.
+//         // imagebox.innerHTML = `<img src="${URL.createObjectURL(input.files[cropIndex])}" id="crop-image" style="width:100%;">`
+//         const img = document.createElement("img");
+//         img.src = URL.createObjectURL(input.files[cropIndex]);
+//         img.id = `crop-image`;
 
-        imagebox.appendChild(img);
+//         imagebox.appendChild(img);
     
-        // Storing that cropping view image in a variable
-        // const image = document.getElementById('crop-image')
-        // console.log(image);
+//         // Storing that cropping view image in a variable
+//         // const image = document.getElementById('crop-image')
+//         // console.log(image);
     
-        // Displaying the image box
-        document.getElementById('image-cropbox').style.display = 'block'
-        // Displaying the Crop buttton
-        document.getElementById('crop-btn').style.display = 'block'
-        // Hiding the Post button
-        document.getElementById('confirm-btn').style.display = 'none'
+//         // Displaying the image box
+//         document.getElementById('image-cropbox').style.display = 'block'
+//         // Displaying the Crop buttton
+//         document.getElementById('crop-btn').style.display = 'block'
+//         // Hiding the Post button
+//         document.getElementById('confirm-btn').style.display = 'none'
     
-        // Creating a croper object with the cropping view image
-        // The new Cropper() method will do all the magic and diplay the cropping view and adding cropping functionality on the website
-        // For more settings, check out their official documentation at https://github.com/fengyuanchen/cropperjs
-        const cropper = new Cropper(img, {
-        autoCropArea: 1,
-        viewMode: 1,
-        scalable: false,
-        zoomable: false,
-        movable: false,
-        minCropBoxWidth: 220,
-        minCropBoxHeight: 500,
-        })
-        // When crop button is clicked this event will get triggered
-        crop_btn.addEventListener('click', ()=>{
-        // This method coverts the selected cropped image on the cropper canvas into a blob object
-        cropper.getCroppedCanvas().toBlob((blob)=>{
+//         // Creating a croper object with the cropping view image
+//         // The new Cropper() method will do all the magic and diplay the cropping view and adding cropping functionality on the website
+//         // For more settings, check out their official documentation at https://github.com/fengyuanchen/cropperjs
+//         const cropper = new Cropper(img, {
+//         autoCropArea: 1,
+//         viewMode: 1,
+//         scalable: false,
+//         zoomable: false,
+//         movable: false,
+//         minCropBoxWidth: 220,
+//         minCropBoxHeight: 500,
+//         })
+//         // When crop button is clicked this event will get triggered
+//         crop_btn.addEventListener('click', ()=>{
+//         // This method coverts the selected cropped image on the cropper canvas into a blob object
+//         cropper.getCroppedCanvas().toBlob((blob)=>{
             
-            // Gets the original image data
-            let fileInputElement = document.getElementById('annonce_images');
-            // Make a new cropped image file using that blob object, image_data.name will make the new file name same as original image
-            let file = new File([blob], input.files[cropIndex].name,{type:"image/*", lastModified:new Date().getTime()});
-            // Create a new container
-            let container = new DataTransfer();
-            // Add the cropped image file to the container
-            container.items.add(file);
-            // Replace the original image file with the new cropped image file
-            fileInputElement.files = container.files;
+//             // Gets the original image data
+//             let fileInputElement = document.getElementById('annonce_images');
+//             // Make a new cropped image file using that blob object, image_data.name will make the new file name same as original image
+//             let file = new File([blob], input.files[cropIndex].name,{type:"image/*", lastModified:new Date().getTime()});
+//             // Create a new container
+//             let container = new DataTransfer();
+//             // Add the cropped image file to the container
+//             container.items.add(file);
+//             // Replace the original image file with the new cropped image file
+//             fileInputElement.files = container.files;
     
-            // Hide the cropper box
-            document.getElementById('image-cropbox').style.display = 'none'
-            // Hide the crop button
-            document.getElementById('crop-btn').style.display = 'none'
-            // Display the Post button
-            document.getElementById('confirm-btn').style.display = 'block'
+//             // Hide the cropper box
+//             document.getElementById('image-cropbox').style.display = 'none'
+//             // Hide the crop button
+//             document.getElementById('crop-btn').style.display = 'none'
+//             // Display the Post button
+//             document.getElementById('confirm-btn').style.display = 'block'
     
-            });
-        });
-    }
-})
+//             });
+//         });
+//     }
+// })
